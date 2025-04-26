2f3f7a5b749a9d84f78b4f06e4147d3333ccede7
Yasenbaka <1260352143@qq.com>
2025-04-27 00:50:15 +0800

    初始化

---
+++ src/main/java/com/miaoyu/backend/MiaoyuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.miaoyu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoyuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoyuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miaoyu/backend/MiaoyuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaoyu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoyuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
